adbe099633ec56ff8f98d48121c76610bcf968de
LethDuarte <lethduarte@gmail.com>
2022-11-03 14:25:37 -0300

    editando estilo e script

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,25 @@
-function contar(){
-    var texto = document.getElementsByClassName('texto');
-    var hoje = new Date();
-    var copa = new Date(2022, 10, 20);
-    var umDia = 1000*60*60*24;
-    var falta = Math.abs((hoje - copa))/umDia;
-    
-    console.log(Math.trunc(falta));
+var texto = document.getElementById('texto');
+var hoje = new Date();
+const copa = new Date(2022, 10, 20);
+const umDia = 1000*60*60*24;
+var falta = Math.abs((hoje - copa))/umDia;
+var dia = Math.trunc(falta);
+
+function escrever(){
+    var texto = document.getElementById('texto');
+
+    texto.innerHTML = `Faltam ${dia} dias para a Copa do Mundo do Catar!`
+}
+
+function progresso(){
+    var progresso = document.getElementById('progresso');
+    var porc = dia/0.32;
+
+    progresso.style.setProperty('width', `${porc}%`);
+}
+
+function opacidade(){
+    var imagem = document.getElementById('imagem');
+
+    imagem.style.setProperty('opacity', `${dia}%`);
 }
